fcf20a32381e78fdc0308678e7f6d3fc3c2b313d
alavalasaijanesh-6164 <alavalasaijanesh@gmail.com>
2026-05-27 13:19:02 +0530

    email verification implemented via otp through console implemented(this is develop mode)

--- src/main/java/com/dwb/auth/service/AuthService.java
+++ src/main/java/com/dwb/auth/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.dwb.auth.service;
 
 import com.dwb.auth.dto.RegisterRequest;
+import com.dwb.auth.dto.VerifyEmailOtpRequest;
 import com.dwb.common.dto.BaseResponse;
 
 public interface AuthService {
+
     BaseResponse<Object> register(RegisterRequest request);
+
+    BaseResponse<Object> verifyEmailOtp(VerifyEmailOtpRequest request);
 }
