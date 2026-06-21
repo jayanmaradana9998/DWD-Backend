8baabb22f528389574fd5d0355f032119976d013
alavalasaijanesh-6164 <alavalasaijanesh@gmail.com>
2026-06-21 16:30:55 +0530

    update:check

--- src/main/java/com/dwb/warranty/controller/WarrantyController.java
+++ src/main/java/com/dwb/warranty/controller/WarrantyController.java
@@ -11,7 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.List; 
 
 @RestController
 @RequestMapping("/api/v1/warranties")
